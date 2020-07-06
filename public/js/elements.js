5f839f27f5842e56c653c787eeb42875d4b447c2
Pablo Bonilla <pbonilla29@hotmail.com>
2020-07-06 02:28:18 -0600

    Add controls hide animation on idle

--- public/js/elements.js
+++ public/js/elements.js
@@ -2,3 +2,4 @@ let video = document.getElementById('mainVideo');
 let controlPlay = document.getElementById('controlPlay');
 let controlPause = document.getElementById('controlPause');
 let controlFullscreen = document.getElementById('controlFullscreen');
+let vp_controlsMaincontainer = document.getElementById('vp_controls-maincontainer');
--- public/js/video_handler.js
+++ public/js/video_handler.js
@@ -1,3 +1,30 @@
+window.onload = () => inactivityTime();
+
+let inactivityTime = function () {
+    let time;
+    window.onload = resetTimer;
+
+    document.onload = resetTimer;
+    document.onmousemove = resetTimer;
+    document.onmousedown = resetTimer;
+    document.ontouchstart = resetTimer;
+    document.onclick = resetTimer;
+    document.onkeypress = resetTimer;
+
+    function showProgressBar() {
+        vp_controlsMaincontainer.style.transform = 'translateY(100%)';
+        // vp_controlsMaincontainer.style.opacity = 0;
+    }
+
+    function resetTimer() {
+        clearTimeout(time);
+        time = setTimeout(showProgressBar, 3000);
+    }
+};
+
+
+
+
 controlPlay.addEventListener('click', () => {
     playVideo();
 });
